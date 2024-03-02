6d0d0e87ce36a216da74f8c8df2fab14537ce663
Lucas Del <deleportelucas44@gmail.com>
2024-03-02 13:19:49 +0100

    Début du formulaire pour les taches

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const create_task = document.querySelector(".create_task");
 const add_task = document.querySelector(".add_task");
-const form = document.querySelector("form");
 const tasks_container = document.querySelector(".tasks-container");
 let taskValue;
 
 create_task.addEventListener("click", () => {
-  document.body.innerHTML += `
-    <form>
-      <input
-      type="text"
-      autocomplete="off"
-      placeholder="Ecrire une tâche"
-      class="add_task"/>
-      <input type="submit" value="Ajouter" />
-    </form>
-  `;
+  // popup form
+  const form = document.querySelector("form");
+  form.style.display = "block";
+
+  add_task.addEventListener("input", (e) => {
+    taskValue = e.target.value;
+  });
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    tasks_container.innerHTML += `
+    <div class="task"></div>
+    `;
+  });
+
+  // form hidden
+  const close_form = document.querySelector(".close_form");
+  close_form.addEventListener("click", (e) => {
+    e.preventDefault();
+    form.style.display = "none";
+  });
 });
