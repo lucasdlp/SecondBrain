0a4800fbedf98f6cfa2a17cd29d8636f5bfbc3fa
Lucas Del <deleportelucas44@gmail.com>
2024-03-05 13:06:40 +0100

    Avancement des fonctionnalitées des taches

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const create_task = document.querySelector(".create_task");
 const add_task = document.querySelector(".add_task");
+const title_task = document.querySelector(".title_task");
 const tasks_container = document.querySelector(".tasks-container");
 let taskValue;
+let titleTask;
 
 add_task.addEventListener("input", (e) => {
   taskValue = e.target.value;
+});
+
+title_task.addEventListener("input", (e) => {
+  titleTask = e.target.value;
 });
 
 create_task.addEventListener("click", () => {
@@ -12,23 +18,35 @@ create_task.addEventListener("click", () => {
   const form = document.querySelector("form");
   form.style.display = "block";
 
+  // create task
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     if (taskValue.length > 0) {
       form.style.display = "none";
       tasks_container.innerHTML += `
       <div class="task">
+        <button class="task_menu"><img src="./assets/svg/task_menu.svg" 
+        alt="menu de la tache" /></button>
+        <p>${titleTask}</p>
         <p>${taskValue}</p>
-        <p>${input_date.value.split("-").reverse().join(" ")}</p>
+        <p>${input_date.value.split("-").reverse().join("/")}</p>
       </div>
     `;
       add_task.value = "";
       taskValue = "";
-    } else {
-      alert("Veuillez entrer une tâche valide !");
-      tasks_container.innerHTML += "";
+      title_task.value = "";
+      titleTask = "";
     }
   });
+
+  // priority
+
+  priority.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.querySelector(".task").classList.add("priority1");
+  });
+
+  // task menu
 
   // form hidden
   const close_form = document.querySelector(".close_form");
